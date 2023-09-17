b73fe627df8103ee64548323b46a6c4e1686be4b
Jorge Martinez <tnlcomputer@gmail.com>
2023-09-17 13:16:43 -0300

    Video 8 y 9 Controller y Metodos magicos - pedir los datos a la base

--- src/Entity/Post.php
+++ src/Entity/Post.php
@@ -11,142 +11,158 @@ use Doctrine\ORM\Mapping as ORM;
 #[ORM\Entity(repositoryClass: PostRepository::class)]
 class Post
 {
-    #[ORM\Id]
-    #[ORM\GeneratedValue]
-    #[ORM\Column]
-    private ?int $id = null;
+  #[ORM\Id]
+  #[ORM\GeneratedValue]
+  #[ORM\Column]
+  private ?int $id = null;
 
-    #[ORM\Column(length: 255)]
-    private ?string $titulo = null;
+  #[ORM\Column(length: 255)]
+  private ?string $titulo = null;
 
-    #[ORM\Column(type: Types::TEXT, nullable: true)]
-    private ?string $type = null;
+  #[ORM\Column(type: Types::TEXT, nullable: true)]
+  private ?string $type = null;
 
-    #[ORM\Column(length: 255, nullable: true)]
-    private ?string $file = null;
+  #[ORM\Column(length: 255, nullable: true)]
+  private ?string $file = null;
 
-    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
-    private ?\DateTimeInterface $creation_date = null;
+  #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+  private ?\DateTimeInterface $creation_date = null;
 
-    #[ORM\Column(length: 255, nullable: true)]
-    private ?string $url = null;
+  #[ORM\Column(length: 255, nullable: true)]
+  private ?string $url = null;
 
-    #[ORM\ManyToOne(inversedBy: 'posts')]
-    #[ORM\JoinColumn(nullable: false)]
-    private ?User $user = null;
+  #[ORM\ManyToOne(inversedBy: 'posts')]
+  #[ORM\JoinColumn(nullable: false)]
+  private ?User $user = null;
 
-    #[ORM\OneToMany(mappedBy: 'post', targetEntity: Interaction::class, orphanRemoval: true)]
-    private Collection $interactions;
+  #[ORM\OneToMany(mappedBy: 'post', targetEntity: Interaction::class, orphanRemoval: true)]
+  private Collection $interactions;
 
-    public function __construct()
-    {
-        $this->interactions = new ArrayCollection();
+  #[ORM\Column(type: Types::TEXT, nullable: true)]
+  private ?string $description = null;
+
+  public function __construct()
+  {
+    $this->interactions = new ArrayCollection();
+  }
+
+  public function getId(): ?int
+  {
+    return $this->id;
+  }
+
+  public function getTitulo(): ?string
+  {
+    return $this->titulo;
+  }
+
+  public function setTitulo(string $titulo): static
+  {
+    $this->titulo = $titulo;
+
+    return $this;
+  }
+
+  public function getType(): ?string
+  {
+    return $this->type;
+  }
+
+  public function setType(?string $type): static
+  {
+    $this->type = $type;
+
+    return $this;
+  }
+
+  public function getDescription(): ?string
+  {
+    return $this->description;
+  }
+
+  public function setDescription(?string $description): static
+  {
+    $this->description = $description;
+
+    return $this;
+  }
+
+  public function getFile(): ?string
+  {
+    return $this->file;
+  }
+
+  public function setFile(?string $file): static
+  {
+    $this->file = $file;
+
+    return $this;
+  }
+
+  public function getCreationDate(): ?\DateTimeInterface
+  {
+    return $this->creation_date;
+  }
+
+  public function setCreationDate(\DateTimeInterface $creation_date): static
+  {
+    $this->creation_date = $creation_date;
+
+    return $this;
+  }
+
+  public function getUrl(): ?string
+  {
+    return $this->url;
+  }
+
+  public function setUrl(?string $url): static
+  {
+    $this->url = $url;
+
+    return $this;
+  }
+
+  public function getUser(): ?User
+  {
+    return $this->user;
+  }
+
+  public function setUser(?User $user): static
+  {
+    $this->user = $user;
+
+    return $this;
+  }
+
+  /**
+   * @return Collection<int, Interaction>
+   */
+  public function getInteractions(): Collection
+  {
+    return $this->interactions;
+  }
+
+  public function addInteraction(Interaction $interaction): static
+  {
+    if (!$this->interactions->contains($interaction)) {
+      $this->interactions->add($interaction);
+      $interaction->setPost($this);
     }
 
-    public function getId(): ?int
-    {
-        return $this->id;
+    return $this;
+  }
+
+  public function removeInteraction(Interaction $interaction): static
+  {
+    if ($this->interactions->removeElement($interaction)) {
+      // set the owning side to null (unless already changed)
+      if ($interaction->getPost() === $this) {
+        $interaction->setPost(null);
+      }
     }
 
-    public function getTitulo(): ?string
-    {
-        return $this->titulo;
-    }
+    return $this;
+  }
 
-    public function setTitulo(string $titulo): static
-    {
-        $this->titulo = $titulo;
-
-        return $this;
-    }
-
-    public function getType(): ?string
-    {
-        return $this->type;
-    }
-
-    public function setType(?string $type): static
-    {
-        $this->type = $type;
-
-        return $this;
-    }
-
-    public function getFile(): ?string
-    {
-        return $this->file;
-    }
-
-    public function setFile(?string $file): static
-    {
-        $this->file = $file;
-
-        return $this;
-    }
-
-    public function getCreationDate(): ?\DateTimeInterface
-    {
-        return $this->creation_date;
-    }
-
-    public function setCreationDate(\DateTimeInterface $creation_date): static
-    {
-        $this->creation_date = $creation_date;
-
-        return $this;
-    }
-
-    public function getUrl(): ?string
-    {
-        return $this->url;
-    }
-
-    public function setUrl(?string $url): static
-    {
-        $this->url = $url;
-
-        return $this;
-    }
-
-    public function getUser(): ?User
-    {
-        return $this->user;
-    }
-
-    public function setUser(?User $user): static
-    {
-        $this->user = $user;
-
-        return $this;
-    }
-
-    /**
-     * @return Collection<int, Interaction>
-     */
-    public function getInteractions(): Collection
-    {
-        return $this->interactions;
-    }
-
-    public function addInteraction(Interaction $interaction): static
-    {
-        if (!$this->interactions->contains($interaction)) {
-            $this->interactions->add($interaction);
-            $interaction->setPost($this);
-        }
-
-        return $this;
-    }
-
-    public function removeInteraction(Interaction $interaction): static
-    {
-        if ($this->interactions->removeElement($interaction)) {
-            // set the owning side to null (unless already changed)
-            if ($interaction->getPost() === $this) {
-                $interaction->setPost(null);
-            }
-        }
-
-        return $this;
-    }
 }
