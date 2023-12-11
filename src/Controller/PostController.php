810226ac9850e98347e651b1de6c0a393d9ffa37
Jorge Martinez <tnlcomputer@gmail.com>
2023-12-11 20:43:33 -0300

    Video 14 - Buenas Practicas

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -2,7 +2,6 @@
 
 namespace App\Controller;
 
-use DateTime;
 use App\Entity\Post;
 use App\Entity\User;
 use App\Form\PostType;
@@ -28,10 +27,14 @@ class PostController extends AbstractController
   public function index(Request $request): Response
   {
     $post = new Post();
+    $posts =  $this->em->getRepository(Post::class)->findAllPost();
+
     $form = $this->createForm(PostType::class, $post);
     $form->handleRequest($request);
 
     if ($form->isSubmitted() && $form->isValid()) {
+      $url = str_replace( " ", "-", $form->get('title')->getData());
+      $post->setUrl($url);
       $user = $this->em->getRepository(User::class)->find(1);
       $post->setUser($user);
       $this->em->persist($post);
@@ -40,47 +43,8 @@ class PostController extends AbstractController
     }
 
     return $this->render('post/index.html.twig', [
-      'form' => $form->createView()
+        'form' => $form->createView(),
+        'posts' => $posts
     ]);
   }
-
-  // #[Route('/post/{id}', name: 'app_post')]
-  // public function index($id): Response
-  // {
-  //   $posts = $this->em->getRepository(Post::class)->find($id);
-  //   $custom_post = $this->em->getRepository(Post::class)->findPost($id);
-  //   return $this->render('post/index.html.twig', [
-  //     'posts' => $posts,
-  //     'custom_post' => $custom_post
-  //   ]);
-  // }
-
-  // #[Route('/insert/post', name: 'insert_post')]
-  // public function insert()
-  // {
-  //   $post = new Post(title: 'Mi post insertado', type: 'opinion', description: 'Hola Mundo', file: 'holita.jpg', url: 'hola mundo');
-  //   $user = $this->em->getRepository(User::class)->find(id: 1);
-  //   $post->setUser($user);
-
-  //   $this->em->persist($post);
-  //   $this->em->flush();
-  //   return new JsonResponse(['Sucsess' => true]);
-  // }
-  // #[Route('/update/post', name: 'insert_post')]
-  // public function update()
-  // {
-  //   $post = $this->em->getRepository(Post::class)->find(id: 2);
-  //   $post->setTitle('Mi post actualizado');
-  //   $this->em->flush();
-  //   return new JsonResponse(['Sucsess' => true]);
-  // }
-
-  // #[Route('/remove/post', name: 'insert_post')]
-  // public function remove()
-  // {
-  //   $post = $this->em->getRepository(Post::class)->find(id: 2);
-  //   $this->em->remove($post);
-  //   $this->em->flush();
-  //   return new JsonResponse(['Sucsess' => true]);
-  // }
 }
